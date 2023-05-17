8c4688c0690b1919b571fea5ecc25a9f9c048b97
Gerhard Keuck <gerhard@metalmanagementsolutions.com>
2023-05-17 17:31:37 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Empire Builder Server
 
+## How to run a solution
+1. Navigate to src or test.
+2. Run with `dotnet run`.
+
---
+++ src/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace sample1 {
+    class EntryClass {
+        public static void Main() {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
